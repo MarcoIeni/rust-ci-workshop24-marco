7342f7e84ac46dfcda8ab46d68dcb3b45375ef71
MarcoIeni <11428655+MarcoIeni@users.noreply.github.com>
2024-11-22 20:36:32 +0100

    add point

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,12 @@
+#[allow(dead_code)]
+
+/// Just a point
+pub struct Point {
+    x: i64,
+    y: i64,
+}
+
+/// Adds two [`Point`]s together.
 pub fn add() {
     let var1 = Some(1);
     if let Some(num) = var1 {
