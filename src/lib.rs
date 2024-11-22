97f7ecd0e3fe249196433302df65e6f8b68d87b1
MarcoIeni <11428655+MarcoIeni@users.noreply.github.com>
2024-11-22 20:06:05 +0100

    fix clippy

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
 pub fn add() {
     let var1 = Some(1);
-    match var1 {
-        Some(num) => println!("{num}"),
-        None => {},
+    if let Some(num) = var1 {
+        println!("{num}")
     }
 }
